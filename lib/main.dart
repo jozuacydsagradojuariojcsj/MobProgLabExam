f056f7af5a5648cb2d3286a08aec7b966bc7874f
JJ <jozuacydjuarioJCSJ@gmail.com>
2023-11-24 23:50:39 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  List names=['Jonathan Juario', 'Maria Corazon Juario','Jozua Cyd Juario'];
+
+  int countLetters(str){
+    int counting = str.length;
+    return counting;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          leading: Icon(Icons.message),
+          title: Text("ListView Builder"),
+        ),
+        body: ListView(
+          children:[
+            for(String name in names)
+              ListTile(
+                leading:Icon(Icons.lock),
+                subtitle:Text(countLetters(name).toString()+' '+"letters"),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
